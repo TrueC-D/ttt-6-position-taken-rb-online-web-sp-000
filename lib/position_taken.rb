d3007359e42767612171a006e970a72e7226b178
Alethea Cary <aletheacary@gmail.com>
2019-11-13 13:37:52 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index].strip.length == 0
+  if board[index].strip.length == 0 || nil
     FALSE
   else
     TRUE
